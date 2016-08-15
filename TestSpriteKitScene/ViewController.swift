0fc9e326b4af675294c1946a53d45e526c913857
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-08-15 06:22:42 -0400

    Test

--- TestSpriteKitScene/ViewController.swift
+++ TestSpriteKitScene/ViewController.swift
@@ -3,19 +3,22 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
+    
+    //最初からあるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //SKViewを取得する。
+        let skView = self.view as! SKView
+        
+        //SKSファイルを指定してSKSceneインスタンスを生成する。
+        let scene = TestScene(fileNamed: "TestScene")
+        
+        //現在シーンを設定する。
+        skView.presentScene(scene)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
